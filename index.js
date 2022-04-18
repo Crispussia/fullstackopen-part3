5e1a7feb7980e1232dfe1cb28a0dc825802aa4e9
Crispussia <degbelocrispussia24@yahoo.com>
2022-04-18 10:18:45 +0300

    part3-C

--- index.js
+++ index.js
@@ -15,28 +15,6 @@ morgan.token('type', (req) =>{
 
 app.use(morgan(':method :url :status  :res[content-length] - :response-time ms :type '))
 
-let persons= [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
@@ -108,18 +86,7 @@ app.delete('/api/persons/:id', (request, response,next) => {
     })
     .catch(error => next(error))
 })
-/*app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
-    response.json(updatedPerson)
-  }).catch(error => next(error))
-
-})*/
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
@@ -134,17 +101,6 @@ app.put('/api/persons/:id', (request, response, next) => {
   })
   .catch(error => next(error))
 })
-/*const generateId = () => {
-  const minId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  const idRandom=Math.floor(Math.random() * (1000 - minId + 1) + minId); 
-  
-    return idRandom
-  
-}
-  */
-
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
