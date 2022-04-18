9fa92521e10f4ec1131ab2bb6d9fb03e2b5ece1a
Crispussia <degbelocrispussia24@yahoo.com>
2022-04-18 03:43:34 +0300

    mongodb2

--- index.js
+++ index.js
@@ -36,10 +36,22 @@ let persons= [
     }
 ]
 
+app.post('/api/persons', (req, res, next) => {
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number
+  })
+
+  newPerson.save().then(response => {
+    res.json(response)
+  }).catch(error => next(error))
+})
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
  
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -47,21 +59,29 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-Person.find({}).then(people => {
-  response.json(people.map(person => person.toJSON()))
-})
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+  /*  const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
     if (person) {
       response.json(person)
     } else {
       response.status(404).end()
+    }*/
+
+    Person.findById(request.params.id)
+  .then(person => {
+    if(person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
     }
   })
+  
+})
 
 
 app.delete('/api/persons/:id', (request, response) => {
+  
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id) 
     response.status(204).end()
@@ -77,32 +97,7 @@ app.delete('/api/persons/:id', (request, response) => {
    
   }
   
-app.post('/api/persons', (request, response) => {
-  const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  else if(persons.map(person => person.name).includes(body.name)) { 
-      return response.status(400).json({
-          error: 'name must be unique'
-      })
-    }
-
-
-  const person = {
-      id: generateId(),
-    name: body.name,
-    number:body.number
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
-})
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
