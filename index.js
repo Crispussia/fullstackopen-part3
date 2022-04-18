b2ded631303b0e2b63a1896c9bc22f4636b621b7
Crispussia <degbelocrispussia24@yahoo.com>
2022-04-18 10:04:14 +0300

    mongodb5

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {runValidators: true, context: 'query', new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
   .then(updatedPerson => {
     response.json(updatedPerson.toJSON())
   })
